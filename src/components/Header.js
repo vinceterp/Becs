fc8a34268060bf41bdf9a7bd852f1bc15c347fc1
Paul <32608382+vinceterp@users.noreply.github.com>
2020-05-24 00:52:15 -0500

    recent ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 //import '../css/style.css';
 import PropTypes from 'prop-types';
 
-
 export default class Header extends React.Component{
     static propTypes= {
         tagline: PropTypes.string,
@@ -11,10 +10,10 @@ export default class Header extends React.Component{
     render(){
         return <header className="top">
             <h1>BECS
-                <span className="ofThe">
-                    <span className="of"></span> 
-                    <span className="the"></span>
-                </span>
+                {/* <span className="ofThe">
+                    <span className="of">of</span> 
+                    <span className="the">the</span> 
+                </span> */}
             </h1>
             <h3 className="tagline">
                 <span>{this.props.tagline}</span>
