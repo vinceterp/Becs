a12ed30fbc65ab2a03e3f8fc32144ddd75b60f0a
Paul <32608382+vinceterp@users.noreply.github.com>
2020-05-24 13:16:57 -0500

    recent ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import '../CSS/App.css';
-<<<<<<< HEAD
 import * as sampleBooks from '../sample-books.js';
 import Header from './Header.js';
 import Book from './Book.js';
-=======
-import books from '../sample-books.js';
->>>>>>> bc01be8a4b88a16d807e8ea1c53a8520dfd853d8
 
 export default class App extends React.Component {
 
@@ -17,7 +13,6 @@ export default class App extends React.Component {
     render(){
         return (
             <div className="menu--container">
-<<<<<<< HEAD
                 {/* {console.log(Object.keys(this.state.books.default))} */}
                 <div className= 'book-list'>
                     <Header tagline= {'Book E-Commerce System'} />
@@ -31,10 +26,6 @@ export default class App extends React.Component {
                 <div className= 'inventory'>
 
                 </div>
-=======
-                {console.log(this.state.sampleBooks)}
-                <p>Hi, I'm a placeholder</p>
->>>>>>> bc01be8a4b88a16d807e8ea1c53a8520dfd853d8
             </div>
         )
     }
