f964ef92f32d30a533824c6fb578ffce7525a59a
Paul <32608382+vinceterp@users.noreply.github.com>
2020-05-26 08:51:34 -0500

    login authentication admin fix

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -18,18 +18,20 @@ export default class Inventory extends React.Component{
     state= {
         uid: null,
         owner: null,
+        
     }
 
     authHandler = async authData=> {
         if (!authData) return;
-        const books= await base.fetch('books', {context: this});
-        console.log(books);
+        const books= await base.fetch('/books', {context: this});
+        console.log(books.owner);
         
         if (!books.owner){
-            await base.post(`/owner`, {data: authData.user.uid||authData.uid});
-            this.setState({uid: authData.user.uid || authData.uid, owner: books.owner || authData.user.uid || authData.uid});
+            await base.post(`/books/owner`, {data: authData.user.uid||authData.uid});
+            this.setState({uid: authData.user.uid || authData.uid, owner: books.owner || authData.user.uid || authData.uid, });
         }
         this.setState({uid: authData.user.uid || authData.uid});
+        this.setState({owner: books.owner});
     }
 
     logout= async () => {
@@ -51,17 +53,20 @@ export default class Inventory extends React.Component{
 
     render(){
         const button= <div><button className='book-edit' onClick={this.logout}>SignOut</button></div>
+        const authenticatedAdmin= this.state.uid === this.state.owner;
 
         if (!this.state.uid){
             return <Login authenticate={this.authenticate}/>
-        }
-       
-        return <div className= "Inventory">Inventory
+        }else if (authenticatedAdmin){
+            return <div className= "Inventory">Inventory
                     <AddBookForm addBook={this.props.addBookToState}/>
                     <button className= "book-edit" onClick={this.props.loadSamples}>Load Sample Books</button>
                     {button}
                     {Object.entries(this.props.books).map((book)=> {return <EditBookForm key= {book.barcode} deleteBook= {this.props.deleteBook} updateBook= {this.props.updateBook} bookDetails= {book}/>})}
                     {button}
                 </div>
+        }else{
+            return <Login authenticate={this.authenticate}/>
+        }     
     }
 }
